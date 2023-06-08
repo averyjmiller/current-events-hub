7704641933816553b9c7b0ac6a78deb58e1fce4c
Avery Miller <averymiller2060@gmail.com>
2023-06-08 19:07:55 -0400

    linked js file in html

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   var mediaKey = "877242f5effa43a7d5b13f42c4c74257";
 
   function renderFeaturedNews() {
-		var mediaUrl = "http://api.mediastack.com/v1/news?access_key=" + mediaKey + "&sort=published_asc";
+		var mediaUrl = "http://api.mediastack.com/v1/news?access_key=" + mediaKey + "&sort=popularity";
+
+		fetch(mediaUrl)
+			.then(function (response) {
+				if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to API');
+			});
 	}
 
 	renderFeaturedNews();
