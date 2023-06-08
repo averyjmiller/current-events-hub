c49198027488027592ad83be2c5a92da4dcf203f
Avery Miller <averymiller2060@gmail.com>
2023-06-08 19:39:03 -0400

    added functions to fetch the featured news articles

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
+	// Key for mediastack API
   var mediaKey = "877242f5effa43a7d5b13f42c4c74257";
 
-  function renderFeaturedNews() {
-		var mediaUrl = "http://api.mediastack.com/v1/news?access_key=" + mediaKey + "&sort=published_asc";
+	// Fetch request function
+  function fetchFeaturedNews() {
+		var mediaUrl = "http://api.mediastack.com/v1/news?access_key=" + mediaKey + "&countries=us&languages=en&sort=popularity&limit=2";
+
+		fetch(mediaUrl)
+			.then(function (response) {
+				if (response.ok) {
+          response.json().then(function (data) {
+						renderFeaturedNews(data.data)
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to API');
+			});
 	}
 
-	renderFeaturedNews();
+	function renderFeaturedNews(news) {
+
+	}
+
+	fetchFeaturedNews();
 
 });
