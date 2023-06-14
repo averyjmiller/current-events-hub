3c5d0559d0e91070ac33cf0b146a99a8bd7407a5
Mona Lisa <krmproductions19@gmail.com>
2023-06-13 22:52:16 -0400

    Updating weather homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -140,6 +140,7 @@ async function storeIp(){
   var ip = await response.json();
   var location = JSON.stringify(ip.loc)
   var getCity = JSON.stringify(ip.city)
+  
 
   sliceCity();
   // Taking the quotes off the city name and storing it
@@ -154,64 +155,75 @@ async function storeIp(){
    var partOne = x.slice(0, x.length / 2)
    var partTwo = x.slice(x.length / 2, x.length)
    var partTwoCut = partTwo.replace(",", "")
-   console.log(partOne)
-   console.log(partTwoCut)
 
     // Slice Lat
     if(partOne[1] === "-"){
       var newLat = partOne.slice(1, 7)
       localStorage.setItem("Latitude", newLat)
-      console.log(newLat)
     } else {
       var newLat = partOne.slice(1, 6)
       localStorage.setItem("Latitude", newLat)
-      console.log(newLat)
     }
   
    // Slice Lon
    if(partTwoCut.startsWith("-")){
     var newLon = partTwoCut.slice(0, 6);
     localStorage.setItem("Longitude", newLon)
-    console.log(newLon);
    } else if(partTwoCut.startsWith(",")){
     var newLon = partTwoCut.slice(1, 6);
     localStorage.setItem("Longitude", newLon)
-    console.log(newLon);
    } else {
     var newLon = partTwoCut.slice(0, 5);
     localStorage.setItem("Longitude", newLon)
-    console.log(newLon);
    }
-
-   return console.log(newLat+ " " + newLon);
-
   }
 
-  cutIp('"-39.5401,-82.4071"');
+  cutIp(location);
 
   // Getting lat and lon from localstorage to send into weather Api request
   var Latitude = localStorage.getItem("Latitude")
-  console.log(Latitude)
   var Longitude = localStorage.getItem("Longitude")
-  console.log(Longitude)
 
-  fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + Latitude + "&lon=" + Longitude + "&appid=0f14b0df581c6adeaefe66badf8a8ffd")
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + Latitude + "&lon=" + Longitude + "&appid=0f14b0df581c6adeaefe66badf8a8ffd&units=imperial")
    .then(function (response) {
     return response.json();
    })
    .then(function (data) {
     localStorage.setItem("Weather", JSON.stringify(data))
    })
-   
-   var newCity = localStorage.getItem("City")
+
+   // Weather object
    var getWeather = JSON.parse(localStorage.getItem("Weather"))
-   console.log(getWeather)
-   console.log(newCity)
+
+   // Date
+   var timeStamp = getWeather.dt
+   var day = new Date(timeStamp*1000);
+   var date = (day.toDateString())
+   var dateTag = document.getElementById("date")
+   dateTag.textContent = date
+
+   // Region
+   var region = ip.region
+   
+   // City text
+   var newCity = localStorage.getItem("City")
+   var cityName = document.getElementById("city1")
+   cityName.textContent = newCity + (", ") + region + (".")
+
+   // Temp text
+   var temp = document.getElementById("temp")
+   temp.textContent = getWeather.main.temp + ("°F")
+
+   // Humidity
+   var humidity1 = document.getElementById("humidity1")
+   humidity1.textContent = getWeather.main.humidity + ("%")
+
+   // Wind speed
+   var windSpeed1 = document.getElementById("windspeed")
+   windSpeed1.textContent = getWeather.wind.speed + (" Mph")
+   
 }
-
-getIp();
-
-
+ getIp();
 
 });
 
